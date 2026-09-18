3a8d6dedc2d6a3cbb08fee82b24476f9efd7feca
Nabeel <nabeelpasha74@gmail.com>
2026-09-18 23:45:31 +0530

    deployment issues

--- backend/app/main.py
+++ backend/app/main.py
@@ -18,7 +18,10 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=[
+        "http://localhost:5173",
+        "https://stm-7nq9.vercel.app",
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
