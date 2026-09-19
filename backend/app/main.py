09d4935a77408cd69447fa75d60edb613a83a8d9
Nabeel <nabeelpasha74@gmail.com>
2026-09-19 09:25:50 +0530

    updated domains in cors for deployment

--- backend/app/main.py
+++ backend/app/main.py
@@ -17,14 +17,13 @@ app = FastAPI()
 
 
 app.add_middleware(
+   app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        "http://localhost:5173",
-        "https://stm-wqky.vercel.app/",
-    ],
+    allow_origins=["https://stm-wqky.vercel.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
+)
 )
 
 
