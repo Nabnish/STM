eedff59ac83844dcd0f59f1101025b232dca3f1b
Nabeel <nabeelpasha74@gmail.com>
2026-09-19 09:30:43 +0530

    cors syntax error

--- backend/app/main.py
+++ backend/app/main.py
@@ -17,13 +17,13 @@ app = FastAPI()
 
 
 app.add_middleware(
-   app.add_middleware(
+  
     CORSMiddleware,
     allow_origins=["https://stm-wqky.vercel.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
-)
+
 )
 
 
